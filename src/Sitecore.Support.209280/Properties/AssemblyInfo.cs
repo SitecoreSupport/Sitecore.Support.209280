42e117ca4339d63eeb7beee1e8649877919abdbf
DK\dmsa <dmsa@sitecore.net>
2018-02-27 13:20:57 +0200

    #207280: Fix the invalid number of days in DataField field type

---
+++ src/Sitecore.Support.209280/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.209280")]
+[assembly: AssemblyProduct("Sitecore.Support.209280")]
+[assembly: ComVisible(false)]
